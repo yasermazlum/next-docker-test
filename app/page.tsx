6493ed213c058a0adeeb109c283b4dd9eda26a37
Yaser <yasermazlum@gmail.com>
2023-06-14 20:29:24 +0300

    ssr

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-"use-client"
-
 export const getData = async ():Promise<Array<{id:number, title:string}>> =>{
   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
   const data:Array<{id:number, title:string}> = await res.json();
